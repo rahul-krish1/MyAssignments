045c5f259734ee2a36ad932c60369248a800179a
rahul-krish1 <118722974+rahul-krish1@users.noreply.github.com>
2022-11-22 08:41:50 -0800

    Add files via upload
    Week1 Day2

---
+++ day2/CheckIfTheNumberIsPositiveOrNegative.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class CheckIfTheNumberIsPositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 0;
+		
+		if(n>0)
+		{
+			System.out.println(n + " is Positive");
+		}
+		
+		else if(n<0)
+		{
+			System.out.println(n + " is Negative");
+		}
+		
+		else
+		{
+			System.out.println(n + " is neither negative or positive");
+		}
+
+	}
+
+}
