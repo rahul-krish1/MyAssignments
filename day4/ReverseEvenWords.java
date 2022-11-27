d35d3190c443a49a24d38519f8caf411ca3d71c7
rahul-krish1 <118722974+rahul-krish1@users.noreply.github.com>
2022-11-27 09:34:15 -0800

    Add files via upload
    Week1 Day4

---
+++ day4/ReverseEvenWords.java
@@ -0,0 +1,16 @@
+package week1.day4;
+
+public class ReverseEvenWords {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String test = "I am a software tester";
+		
+		String[] split = test.split(test);
+		
+		
+
+	}
+
+}
